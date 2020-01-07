4f6aca08109f715a680b206920045ef7151d710a
steve.k.you <steve.k.you@yzw.cn>
2020-01-07 10:37:59 +0800

    upgrade

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,6 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var bimface_1 = require("./providers/bimface");
-exports.ProviderType = {
-    BIMFACE: 'bimface'
-};
-function getProvider(provider) {
-    switch (provider) {
-        case exports.ProviderType.BIMFACE:
-            return new bimface_1.default();
-        default:
-            throw new Error("provider [" + provider + "] not found");
-    }
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-exports.default = getProvider;
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./src/index"));
