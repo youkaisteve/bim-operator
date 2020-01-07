b7fe6300cfe55cdd2dfcb28e74de17c028f5296d
star.x.lan <star.x.lan@yzw.cn>
2020-01-07 17:03:53 +0800

    bimface 添加锚点接口相关接口

--- src/providers/bimface.ts
+++ src/providers/bimface.ts
@@ -1,5 +1,5 @@
-import { IBimOperation, Floor, Component } from '../interface';
-import remoteLoad from '../util/remote-load';
+import { IBimOperation, Floor, Component, Marker3dStyle } from "../interface";
+import remoteLoad from "../util/remote-load";
 
 /**
  * bimface操作
@@ -11,6 +11,17 @@ export default class Bimface implements IBimOperation {
   app: any;
   viewer3D: any;
   viewer2D: any;
+  marker3D: any;
+  marker3DStyle: Marker3dStyle = {
+    src: "http://static.bimface.com/resources/3DMarker/warner/warner_red.png",
+    size: 30,
+    tooltip: ""
+  };
+  /**
+   * 3d锚点功能开关
+   */
+  is3dMarkerOn = false;
+
   /**
    * 加载模型
    * @param {Object} options
@@ -24,11 +35,11 @@ export default class Bimface implements IBimOperation {
     }
 
     if (!options.domId) {
-      throw new Error('domId missing');
+      throw new Error("domId missing");
     }
 
     if (!options.viewToken) {
-      throw new Error('viewToken missing');
+      throw new Error("viewToken missing");
     }
 
     return new Promise((resolve, reject) => {
@@ -40,7 +51,9 @@ export default class Bimface implements IBimOperation {
           var domShow = document.getElementById(options.domId);
           var webAppConfig = new window.Glodon.Bimface.Application.WebApplication3DConfig();
           webAppConfig.domElement = domShow;
-          this.app = new window.Glodon.Bimface.Application.WebApplication3D(webAppConfig);
+          this.app = new window.Glodon.Bimface.Application.WebApplication3D(
+            webAppConfig
+          );
           this.app.addView(options.viewToken);
           this.viewer3D = this.app.getViewer();
           resolve();
@@ -75,10 +88,93 @@ export default class Bimface implements IBimOperation {
    */
   getComponentByCondition(confition: any): Promise<Component[]> {
     if (!confition.fileId) {
-      throw new Error('fileId不能为空');
+      throw new Error("fileId不能为空");
     }
     return new Promise(resolve => {
-      this.viewer3D.getElementByConditions(confition.fileId, confition, resolve);
+      this.viewer3D.getElementByConditions(
+        confition.fileId,
+        confition,
+        resolve
+      );
     });
+  }
+
+  /**
+   * 开始3d锚点功能
+   */
+  turn3dMarkerOn() {
+    if (!this.marker3D) {
+      const markerConfig = new window.Glodon.Bimface.Plugins.Marker3D.Marker3DContainerConfig();
+      markerConfig.viewer = this.viewer3D;
+      this.marker3D = new window.Glodon.Bimface.Plugins.Marker3D.Marker3DContainer(
+        markerConfig
+      );
+      this.viewer3D.addEventListener(
+        window.Glodon.Bimface.Viewer.Viewer3DEvent.MouseClicked,
+        objectData => {
+          if (this.is3dMarkerOn && objectData && objectData.objectId) {
+            const marker3d = this.add3dMarker(objectData.worldPosition);
+            marker3d.onClick(marker => this.remove3dMarker(marker.id));
+          }
+        }
+      );
+    }
+    this.is3dMarkerOn = true;
+  }
+
+  /**
+   * 关闭3d锚点功能
+   */
+  turn3dMarkerOff() {
+    this.is3dMarkerOn = false;
+  }
+
+  /**
+   * 设置3d锚点配置
+   */
+  set3dMarkerStyle(config) {
+    if (Object.prototype.toString.call(config) !== "[object Object]") {
+      throw new Error("请传入对象");
+    }
+    const { src, size, tooltip } = config;
+    if (typeof src === "string") {
+      this.marker3DStyle.src = src;
+    }
+    if (typeof size === "number") {
+      this.marker3DStyle.size = size;
+    }
+    if (typeof tooltip === "string") {
+      this.marker3DStyle.tooltip = tooltip;
+    }
+  }
+
+  /**
+   * 清空3d锚点
+   */
+  clear3dMarker() {
+    this.marker3D && this.marker3D.clear();
+  }
+
+  /**
+   * 添加3d锚点
+   */
+  private add3dMarker(position: any) {
+    const marker3dConfig = new window.Glodon.Bimface.Plugins.Marker3D.Marker3DConfig();
+    marker3dConfig.worldPosition = position;
+    marker3dConfig.src = this.marker3DStyle.src;
+    marker3dConfig.size = this.marker3DStyle.size;
+    marker3dConfig.tooltip = this.marker3DStyle.tooltip;
+    const marker3d = new window.Glodon.Bimface.Plugins.Marker3D.Marker3D(
+      marker3dConfig
+    );
+    this.marker3D.addItem(marker3d);
+    return marker3d;
+  }
+
+  /**
+   * 移除3d锚点
+   */
+  private remove3dMarker(markerId: number) {
+    this.marker3D.removeItemById(markerId);
   }
 }
