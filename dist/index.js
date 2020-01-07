4269ba5931b171b47bb7ffef6f18d60791da5080
youkaisteve <youkaisteve@gmail.com>
2020-01-07 10:57:19 +0800

    upgrade

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,15 @@
 "use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./src/index"));
+var bimface_1 = require("./providers/bimface");
+exports.ProviderType = {
+    BIMFACE: 'bimface'
+};
+function getProvider(provider) {
+    switch (provider) {
+        case exports.ProviderType.BIMFACE:
+            return new bimface_1.default();
+        default:
+            throw new Error("provider [" + provider + "] not found");
+    }
+}
+exports.default = getProvider;
