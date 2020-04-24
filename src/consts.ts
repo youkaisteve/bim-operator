6001011892251305d2b0ce4dd1cfd06a9915d9ef
steve.k.you <steve.k.you@yzw.cn>
2020-04-24 14:58:45 +0800

    提供unsafe的方式加载sdk

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,5 @@
  * bimface js sdk的地址
  */
 export const BIMFACE_JS_SDK = 'https://static.bimface.com/api/BimfaceSDKLoader/BimfaceSDKLoader@latest-release.js';
+export const BIMFACE_JS_SDK_UNSAFE =
+    'http://static.bimface.com/api/BimfaceSDKLoader/BimfaceSDKLoader@latest-release.js';
